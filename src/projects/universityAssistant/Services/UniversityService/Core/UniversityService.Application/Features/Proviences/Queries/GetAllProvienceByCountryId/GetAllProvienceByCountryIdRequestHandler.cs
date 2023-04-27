6dc398e2fc0e4a6c49f108d7711debf64ebbb7da
Fatih DEMİRCİ <fatih.demirci61@hotmail.com>
2023-04-27 16:58:29 +0300

    ProvienceRepository and GetAllProvienceByCountryId

---
+++ src/projects/universityAssistant/Services/UniversityService/Core/UniversityService.Application/Features/Proviences/Queries/GetAllProvienceByCountryId/GetAllProvienceByCountryIdRequestHandler.cs
@@ -0,0 +1,21 @@
+﻿using Core.Persistence.Paging;
+using MediatR;
+using UniversityService.Application.Services.Repositories;
+
+namespace UniversityService.Application.Features.Proviences.Queries.GetAllProvienceByCountryId;
+
+public class GetAllProvienceByCountryIdRequestHandler : IRequestHandler<GetAllProvienceByCountryIdRequest, IPaginate<GetAllProvienceByCountryIdResponseDto>>
+{
+    private readonly IProvienceRepository _provienceRepository;
+
+    public GetAllProvienceByCountryIdRequestHandler(IProvienceRepository provienceRepository)
+    {
+        _provienceRepository = provienceRepository;
+    }
+
+    public async Task<IPaginate<GetAllProvienceByCountryIdResponseDto>> Handle(GetAllProvienceByCountryIdRequest request, CancellationToken cancellationToken)
+    {
+        IPaginate<GetAllProvienceByCountryIdResponseDto> result = await _provienceRepository.GetListAsync<GetAllProvienceByCountryIdResponseDto>(i => i.CountryId == request.CountryId, index: request.Index, size: request.Size);
+        return result;
+    }
+}
---
+++ src/projects/universityAssistant/Services/UniversityService/Core/UniversityService.Application/Features/Proviences/Queries/GetAllProvienceByCountryId/GetAllProvienceByCountryIdResponseDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UniversityService.Domain.Entities;
+
+namespace UniversityService.Application.Features.Proviences.Queries.GetAllProvienceByCountryId;
+
+public class GetAllProvienceByCountryIdResponseDto
+{
+    public int Id { get; set; }
+    public int CountryId { get; set; }
+    public string Name { get; set; }
+}
