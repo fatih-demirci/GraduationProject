26853b4029ce400fe050e2174fca67b9172cec15
Fatih DEMİRCİ <fatih.demirci61@hotmail.com>
2023-05-13 18:10:19 +0300

    Validators added for AddFaculty and UpdateFaculty

---
+++ src/projects/universityAssistant/Services/UniversityService/Core/UniversityService.Application/Features/Faculties/Commands/AddFaculty/AddFacultyCommandRequestValidator.cs
@@ -0,0 +1,17 @@
+﻿using FluentValidation;
+
+namespace UniversityService.Application.Features.Faculties.Commands.AddFaculty;
+
+public class AddFacultyCommandRequestValidator : AbstractValidator<AddFacultyCommandRequest>
+{
+    public AddFacultyCommandRequestValidator()
+    {
+        RuleForEach(x => x.AddFacultyCommandRequestFaculties)
+            .ChildRules(y => y.RuleForEach(z => z.FacultyCultures)
+            .ChildRules(d =>
+        {
+            d.RuleFor(fc => fc.Culture).MinimumLength(4);
+            d.RuleFor(fc => fc.Name).MinimumLength(5);
+        }));
+    }
+}
---
+++ src/projects/universityAssistant/Services/UniversityService/Core/UniversityService.Application/Features/Faculties/Commands/UpdateFaculty/UpdateFacultyCommandRequestValidator.cs
@@ -0,0 +1,19 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UniversityService.Application.Features.Faculties.Commands.UpdateFaculty;
+
+public class UpdateFacultyCommandRequestValidator : AbstractValidator<UpdateFacultyCommandRequest>
+{
+    public UpdateFacultyCommandRequestValidator()
+    {
+        RuleFor(i => i.Culture).MinimumLength(4);
+        RuleFor(i => i.FacultyCultureId).GreaterThan(0);
+        RuleFor(i => i.Name).MinimumLength(5);
+        RuleFor(i => i.Id).GreaterThan(0);
+    }
+}
